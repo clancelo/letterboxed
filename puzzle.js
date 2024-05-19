b2a82a6cfbf5ca03c441b19da1b52356a0e720d3
Corey Lancelotta <clancelo@asu.edu>
2024-05-18 22:49:39 -0500

    Add new puzzle

--- puzzle.js
+++ puzzle.js
@@ -1,17 +1,17 @@
 // Puzzle structure
 const puzzle = [
-    { "id": 0, "value": 'T' },
-    { "id": 1, "value": 'E' },
-    { "id": 2, "value": 'D' },
-    { "id": 3, "value": 'I' },
-    { "id": 4, "value": 'L' },
-    { "id": 5, "value": 'R' },
-    { "id": 6, "value": 'V' },
+    { "id": 0, "value": 'A' },
+    { "id": 1, "value": 'K' },
+    { "id": 2, "value": 'E' },
+    { "id": 3, "value": 'L' },
+    { "id": 4, "value": 'I' },
+    { "id": 5, "value": 'O' },
+    { "id": 6, "value": 'J' },
     { "id": 7, "value": 'N' },
-    { "id": 8, "value": 'C' },
-    { "id": 9, "value": 'S' },
-    { "id": 10, "value": 'O' },
-    { "id": 11, "value": 'K' }
+    { "id": 8, "value": 'T' },
+    { "id": 9, "value": 'P' },
+    { "id": 10, "value": 'R' },
+    { "id": 11, "value": 'M' }
 ]
 
 export default puzzle;
